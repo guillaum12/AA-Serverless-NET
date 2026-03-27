33bdbec9a6a6d5a10757d4277b6c31502ec075ec
YassineOuchna <129407743+YassineOuchna@users.noreply.github.com>
2026-03-27 13:59:25 +0000

    new deployed

--- ResizeFunction/OuchnaMachabertProject.cs
+++ ResizeFunction/OuchnaMachabertProject.cs
@@ -20,4 +20,44 @@ public class OuchnaMachabertProject
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
+
+    // TODO N'accepter que le POST
+    [FunctionName("ResizeHttpTrigger")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        if (!int.TryParse(req.Query["w"], out int w) ||
+            !int.TryParse(req.Query["h"], out int h))
+        {
+            return new BadRequestObjectResult("Invalid w or h");
+        }
+
+        byte[]  targetImageBytes;
+        using(var  msInput = new MemoryStream())
+        {
+            // Récupère le corps du message en mémoire
+            await req.Body.CopyToAsync(msInput);
+            msInput.Position = 0;
+
+            // Charge l'image       
+            using (var image = Image.Load(msInput)) 
+            {
+                // Effectue la transformation
+                image.Mutate(x => x.Resize(w, h));
+
+                // Sauvegarde en mémoire               
+                using (var msOutput = new MemoryStream())
+                {
+                    image.SaveAsJpeg(msOutput);
+                    targetImageBytes = msOutput.ToArray();
+                }
+            }
+        }
+        // Renvoie le contenu avec le content-type correspondant à une image jpeg
+        // TODO renvoyer les octets de l'image
+        // TODO ... ainsi que le content-type correspondant à une image Jpeg
+        return new FileContentResult(targetImageBytes, "image/jpeg");
+    }
+
 }
